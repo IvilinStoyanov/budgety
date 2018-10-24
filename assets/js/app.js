a3da87169e3b79c4405e93257f3b90b132df67eb
Ivilin Stoyanov <32274614+IvilinStoyanov@users.noreply.github.com>
2018-10-24 18:40:31 +0300

    setting up the delete event listener using event delegation

--- assets/js/app.js
+++ assets/js/app.js
@@ -109,7 +109,8 @@ var UIController = (function () {
         budgetLabel: '.budget__value',
         incomeLabel: '.budget__income--value',
         expensesLabel: '.budget__expenses--value',
-        percentageLabel: '.budget__expenses--percentage'
+        percentageLabel: '.budget__expenses--percentage',
+        container: '.container'
     };
 
     return {
@@ -127,7 +128,7 @@ var UIController = (function () {
             if (type === 'inc') {
                 element = DOMstrings.incomeContainer;
 
-                html = '<div class="item clearfix" id="inc-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+                html = '<div class="item clearfix" id="inc-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type === 'exp') {
                 element = DOMstrings.expensesContainer;
 
@@ -194,6 +195,8 @@ var controller = (function (budgetCtrl, UICtrl) {
                 ctrlAddItem();
             }
         });
+
+        document.querySelector(DOM.container).addEventListener('click' ,ctrDeleteItem);
     };
 
     var updateBudget = function () {
@@ -229,6 +232,15 @@ var controller = (function (budgetCtrl, UICtrl) {
             // Calculate and Update Budget
             updateBudget();
         }
+    };
+
+    var ctrDeleteItem = function(event) {
+
+       // console.log(event.target.parentNode);
+       itemId = event.target.parentNode.parentNode.parentNode.parentNode.id;    
+       if(itemID) {
+
+       }
     };
 
     return {
