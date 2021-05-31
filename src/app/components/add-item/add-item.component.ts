7ab515e2c111b8df3b62c9df0dc36c255047e647
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2021-05-31 18:10:01 +0300

    expand categories

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -1,19 +1,30 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
   styleUrls: ['./add-item.component.scss'],
 })
-export class AddItemComponent implements OnInit {
+export class AddItemComponent implements OnInit, OnDestroy {
+  _categorySubscription: Subscription = new Subscription();
+
   form: FormGroup;
+  categoryPicked: any;
 
   categories: any[] = [
+    { value: { icon: 'attach_money', name: 'Salary' }, viewValue: 'Salary' },
     { value: { icon: 'directions_car_filled', name: 'Car' }, viewValue: 'Car' },
     { value: { icon: 'shopping_cart', name: 'Grocery' }, viewValue: 'Grocery' },
-    { value: { icon: 'attach_money', name: 'Salary' }, viewValue: 'Salary' },
+    { value: { icon: 'restaurant', name: 'Food & Restaurant' }, viewValue: 'Food & Restaurant' },
+    { value: { icon: 'local_cafe', name: 'Coffe' }, viewValue: 'Coffe' },
+    { value: { icon: 'holiday_village', name: 'Holiday' }, viewValue: 'Holiday' },
+    { value: { icon: 'receipt', name: 'Utilities' }, viewValue: 'Utilities' },
+    { value: { icon: 'bedroom_parent', name: 'Rent' }, viewValue: 'Rent' },
+    { value: { icon: 'credit_score', name: 'Loan Payments' }, viewValue: 'Loan Payments' },
+    
   ];
 
   constructor(
@@ -24,6 +35,14 @@ export class AddItemComponent implements OnInit {
 
   ngOnInit() {
     this.createForm();
+
+    var categorySubscription$ =  this.form.get('category').valueChanges.subscribe(value => this.categoryPicked = value?.name);
+
+    this._categorySubscription.add(categorySubscription$);
+  }
+
+  ngOnDestroy() {
+    this._categorySubscription.unsubscribe();
   }
 
   createForm() {
