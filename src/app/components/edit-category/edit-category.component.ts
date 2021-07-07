e63c6dbd154f33ececd2adffd45ed602a86d31b6
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2021-07-07 10:29:07 +0300

    add latest changes

--- src/app/components/edit-category/edit-category.component.ts
+++ src/app/components/edit-category/edit-category.component.ts
@@ -20,19 +20,48 @@ export class EditCategoryComponent implements OnInit {
 
   }
 
-  deleteTemplate(categoryID: number, type: string) {
-    if (type === 'default') this.data.categoryTemplates.splice(categoryID, 1);
+  deleteTemplate(index: number, categoryID: number, type: string) {
+    let category: any;
+    if (type === 'default') {
+      console.log(categoryID);
+      // this.data.categoryTemplates.splice(index, 1);
+      category = this.data.categories.find(c => c.id == categoryID);
+    }
 
-    if (type === 'custom') this.data.categoryTemplatesCustom.splice(categoryID, 1);
+    if (type === 'custom') {
+      //  this.data.categoryTemplatesCustom.splice(index, 1);
+       category = this.data.categories.find(c => c.id == categoryID);
+    }
 
-    this.commonService.saveData(this.data);
+    console.log(category);
+    category.items.forEach(item => {
+      if (item.type === 'exp') {
+        this.data.totals.exp -= item.value;
+      }
+      if (item.type === 'inc') {
+        this.data.totals.inc += item.value;
+      }
+    });
+    
+    this.data.categories[categoryID].inc = 0;
+    this.data.categories[categoryID].exp = 0;
+
+    this.data.budget = this.data.totals.inc - this.data.totals.exp;
+
+    // calculate category income/expense percetanges of current budget
+    this.data = this.commonService.calculateTotalExpPercentage(this.data);
+
+    // calculate global income/expense percetanges of current budget
+    this.data = this.commonService.calculatePercentageEach(this.data);
+
+    //  this.commonService.saveData(this.data);
   }
 
   changeColor(categoryID: number, color: string, type: string) {
     if (type === 'default') this.data.categoryTemplates[categoryID].color = color;
 
     if (type === 'custom') this.data.categoryTemplatesCustom[categoryID].color = color;
-    
+
     this.data.categories[categoryID].color = color;
 
     this.commonService.saveData(this.data);
