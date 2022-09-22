c3c5f2cd91a0aa56f7482ec3f3661efab1392ed5
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-09-22 19:42:56 +0300

    adding mongo url to the heroku env

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+    mongoURL: process.env.MONGO_URL
 }
