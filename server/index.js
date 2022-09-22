3bc4c2644516e45734071733a289209f6652501d
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-09-22 17:52:42 +0300

    setting up google oauth flow on the express server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,31 @@
 const express = require('express');
-// const passport = require('passport');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(bodyParser.json());
+app.use(
+    cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+}));
 
-// require('./routes/authRoutes')(app);
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
     res.send('Budgety API');
-})
+});
 
 app.listen(process.env.PORT || 5000);
