14fd71ec9847bf46d3562c2bc74afa8b625fc9ac
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-09-22 12:50:56 +0300

    initial setup

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// const passport = require('passport');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+// require('./routes/authRoutes')(app);
+
+app.get('/', (req, res) => {
+    res.send('Budgety API');
+})
+
+app.listen(5000);
