e1a6ca2967bf8888c9634d7e3b9f37d54f31653f
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-09-22 22:46:15 +0300

    add redirect domain to heroku

--- server/config/prod.js
+++ server/config/prod.js
@@ -2,5 +2,6 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY,
-    mongoURL: process.env.MONGO_URL
+    mongoURL: process.env.MONGO_URL,
+    redirectDomain: process.env.REDIRECT_DOMAIN
 }
