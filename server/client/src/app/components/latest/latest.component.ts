a1b76665b4f74ada3af963be930d71f1c601c0c5
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-10-08 17:05:55 +0300

    git revert

--- server/client/src/app/components/latest/latest.component.ts
+++ server/client/src/app/components/latest/latest.component.ts
@@ -79,6 +79,7 @@ export class LatestComponent implements OnInit {
   }
 
   openSetupCategoriesModal() {
+
     const dialogRef = this.dialog.open(SetupCategoriesComponent, {
       autoFocus: false,
       disableClose: true
