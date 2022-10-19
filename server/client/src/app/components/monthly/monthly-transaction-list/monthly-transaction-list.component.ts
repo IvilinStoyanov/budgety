0820b89da5165026c3e53686e8446d5dc88181e3
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-10-19 19:24:15 +0300

    refactoring the code using RxJs

--- server/client/src/app/components/monthly/monthly-transaction-list/monthly-transaction-list.component.ts
+++ server/client/src/app/components/monthly/monthly-transaction-list/monthly-transaction-list.component.ts
@@ -1,6 +1,8 @@
 import { ThrowStmt } from '@angular/compiler';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { combineLatest, forkJoin, observable, Subject } from 'rxjs';
+import { mergeMap, switchMap, takeUntil, tap } from 'rxjs/operators';
 import { CategoriesService } from 'src/app/services/categories.service';
 import { TransactionsService } from 'src/app/services/transactions.service';
 
@@ -9,51 +11,52 @@ import { TransactionsService } from 'src/app/services/transactions.service';
   templateUrl: './monthly-transaction-list.component.html',
   styleUrls: ['./monthly-transaction-list.component.scss']
 })
-export class MonthlyTransactionListComponent implements OnInit {
+export class MonthlyTransactionListComponent implements OnInit, OnDestroy {
+  $destroyed = new Subject();
   monthName: any;
   monthlyIncome: number = 0;
   items: any[] = [];
   monthlyCategories: any[] = [];
   panelOpenState: boolean;
 
-
-
   constructor(private route: ActivatedRoute, private categoriesService: CategoriesService, private transactionsService: TransactionsService) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.monthName = params['month'];
+    this.route.params.pipe(
+      takeUntil(this.$destroyed),
+      switchMap(params => {
+        this.monthName = params['month'];
+        const year = this.transactionsService.monthlyYearSelected.getFullYear();
 
-      const year = this.transactionsService.monthlyYearSelected.getFullYear();
-
-      this.createMonthlyTransanctions(year, this.monthName)
-    })
-  }
-
-  createMonthlyTransanctions(year: number, monthName: string) {
-    this.categoriesService.getCategories().subscribe(categories => {
-      this.transactionsService.getMonthlyIndividualTransactions(year, monthName).subscribe(transactions => {
-        categories.forEach((category, categoryIndex) => {
+        return combineLatest([this.categoriesService.getCategories(), this.transactionsService.getMonthlyIndividualTransactions(year, this.monthName)]);
+      }),
+    ).subscribe(
+      ([categories, transactions]) => {
+        categories.forEach(category => {
           if (category.transactionsCount > 0)
             this.monthlyCategories[category._id] = [{ name: category.name, exp: 0, inc: 0, items: [] }];
 
-            transactions.forEach(transaction => {
-              if (transaction._categoryId === category._id) {
-                this.monthlyCategories[transaction._categoryId][0].items.push(transaction);
+          transactions.forEach(transaction => {
+            if (transaction._categoryId === category._id) {
+              this.monthlyCategories[transaction._categoryId][0].items.push(transaction);
 
-                if (transaction.type === 'exp') {
-                  this.monthlyCategories[transaction._categoryId][0].exp += transaction.value;
-                }
-                if (transaction.type === 'inc') {
-                  this.monthlyCategories[transaction._categoryId][0].inc += transaction.value;
-                  this.monthlyIncome += transaction.value;
-                }
-                this.items.push(transaction);
+              if (transaction.type === 'exp') {
+                this.monthlyCategories[transaction._categoryId][0].exp += transaction.value;
               }
-            });
+              if (transaction.type === 'inc') {
+                this.monthlyCategories[transaction._categoryId][0].inc += transaction.value;
+                this.monthlyIncome += transaction.value;
+              }
+              this.items.push(transaction);
+            }
+          });
         });
-        console.log(this.monthlyCategories);
-      })
-    })
+      }
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.$destroyed.next(true);
+    this.$destroyed.unsubscribe();
   }
 }
