e9118d7165150f4593eb1e89e43d11c80124a80c
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2022-10-31 11:53:45 +0200

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleClientID: '493775119187-rmt1o917u77i5vtbr88m38kk01m60oma.apps.googleusercontent.com',
-    googleClientSecret: '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',
-    cookieKey: 'budgety-dev-cookie-secret-key',
-    mongoURL: 'mongodb+srv://budgety-dev:9UN6IflthIq1rA7o@cluster0.pkt6oov.mongodb.net/budgety-dev?retryWrites=true&w=majority',
-    redirectDomain: 'http://localhost:4200'
-}
