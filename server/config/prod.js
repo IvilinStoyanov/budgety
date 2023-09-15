fdf4d73a7bca7d4ab2a2ed771ef8aa2de83d270a
Ivilin Stoyanov <ivilin.stoyanov9@gmail.com>
2023-09-16 00:28:22 +0300

    add missing files

---
+++ server/config/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    cookieKey: process.env.COOKIE_KEY,
+    mongoURL: process.env.MONGO_URL,
+    redirectDomain: process.env.REDIRECT_DOMAIN
+}
---
+++ server/models/Categories.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const categoriesSchema = new Schema({
+    name: String,
+    color: String,
+    icon: String,
+    exp: { type: Number, default: 0 },
+    categoryId: { type: Number, default: 0 },
+    inc: { type: Number, default: 0 },
+    transactionsCount: { type: Number, default: 0 },
+    isVisible: { type: Boolean, default: true },
+    _user: { type: Schema.Types.ObjectId, ref: 'user' }
+});
+
+mongoose.model('categories', categoriesSchema);
